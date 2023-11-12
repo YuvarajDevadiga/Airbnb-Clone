5e10892e57422ea3053d320f77abdfdc70b4b1f1
Yuvaraj Devadiga <100144517+YuvarajDevadiga@users.noreply.github.com>
2023-11-12 15:09:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
+const listingController = require("./controllers/listings.js");
+
 const dbUrl = process.env.ATLASDB_URL;
 
 main()
